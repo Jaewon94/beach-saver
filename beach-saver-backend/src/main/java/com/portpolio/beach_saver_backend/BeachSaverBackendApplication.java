ec89e173f567fb4426c6f7de15c5f4cae3adb975
Jaewon94 <wodnjs3580@gmail.com>
2025-06-06 13:37:35 +0900

    feat: 초기 프로젝트 구조 설정 및 필수 파일 추가
    - Beach-Saver 백엔드 프로젝트의 초기 구조를 설정하고 필요한 파일들을 추가
    - Gradle 빌드 시스템을 위한 설정 파일 및 스크립트 추가
    - .gitignore 파일을 통해 불필요한 파일 및 디렉토리 무시 설정
    - Spring Boot 애플리케이션의 기본 클래스 및 설정 파일 생성
    - VSCode 설정 파일 추가로 개발 환경 통일성 강화
    - 문서화 및 프로젝트 관리에 필요한 HELP.md 파일 추가

---
+++ beach-saver-backend/src/main/java/com/portpolio/beach_saver_backend/BeachSaverBackendApplication.java
@@ -0,0 +1,13 @@
+package com.portpolio.beach_saver_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeachSaverBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeachSaverBackendApplication.class, args);
+	}
+
+}
---
+++ beach-saver-backend/src/test/java/com/portpolio/beach_saver_backend/BeachSaverBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portpolio.beach_saver_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeachSaverBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
